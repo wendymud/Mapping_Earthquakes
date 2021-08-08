cf797152fdf16c5ddd0d56e2a678291e9c0c2aae
Wendy Mudzinski <wendymud@gmail.com>
2021-08-08 00:43:57 -0700

    edited to dark map and yellow radius circle

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,8 +4,15 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillcolor: 'lightyellow',
+    radius: 300
+ }).addTo(map);
+
 // Adding the tileLayer code for the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
